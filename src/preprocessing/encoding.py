40f91352c3326bfb79f03bbab2e4da42912eaafa
deleayeni <148347578+deleayeni@users.noreply.github.com>
2025-02-15 13:35:26 +0100

    successfull tests for preprocessing

--- src/preprocessing/encoding.py
+++ src/preprocessing/encoding.py
@@ -1,21 +1,23 @@
 import pandas as pd
+import category_encoders as ce
 
 def encode_categorical_features(original_data, target_column):
     """
-    Encodes categorical features using One-Hot Encoding.
+    Encodes categorical features using Binary Encoding.
 
     Parameters:
     - original_data (DataFrame): The dataset.
     - target_column (str): The column to exclude from encoding.
 
     Returns:
-    - encoded_data (DataFrame): Transformed dataset with categorical encoding.
+    - encoded_data (DataFrame): Transformed dataset with binary encoding.
     """
     categorical_cols = [col for col in original_data.columns if original_data[col].dtype == "object" and col != target_column]
 
     if categorical_cols:
-        encoded_data = pd.get_dummies(original_data, columns=categorical_cols, drop_first=True)
-        print(f"Categorical features encoded: {categorical_cols}")
+        encoder = ce.BinaryEncoder(cols=categorical_cols, drop_invariant=True)
+        encoded_data = encoder.fit_transform(original_data)
+        print(f"Categorical features encoded using Binary Encoding: {categorical_cols}")
     else:
         encoded_data = original_data.copy()
 
