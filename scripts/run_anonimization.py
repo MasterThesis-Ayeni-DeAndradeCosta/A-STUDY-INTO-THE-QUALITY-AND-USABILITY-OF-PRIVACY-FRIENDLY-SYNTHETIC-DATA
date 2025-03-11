96bb849b04808b86ebf6333d670c77947885c85e
Isabelle Costa <isabelle.costa@kuleuven.be>
2025-03-11 14:09:52 +0100

    anonym jar executed

--- scripts/run_anonimization.py
+++ scripts/run_anonimization.py
@@ -1,6 +1,10 @@
 import subprocess
+import os
 
-jar_path = "C:\Users\isabe\Documents\KUL24_25\MASTER_THESIS\sec_sem\code\thesis_code\thesis_code\A-STUDY-INTO-THE-QUALITY-AND-USABILITY-OF-PRIVACY-FRIENDLY-SYNTHETIC-DATA\src\anonymization\anonymARXij\out\artifacts\anonymARXij.jar"
-process = subprocess.run(["java", "-jar", jar_path], capture_output=True, text=True)
+jar_path = r"C:\Users\isabe\Documents\KUL24_25\MASTER_THESIS\sec_sem\code\thesis_code\thesis_code\A-STUDY-INTO-THE-QUALITY-AND-USABILITY-OF-PRIVACY-FRIENDLY-SYNTHETIC-DATA\src\anonymization\anonymARXij\src\executable\anonymARXij.jar"
+working_directory = r"C:\Users\isabe\Documents\KUL24_25\MASTER_THESIS\sec_sem\code\thesis_code\thesis_code\A-STUDY-INTO-THE-QUALITY-AND-USABILITY-OF-PRIVACY-FRIENDLY-SYNTHETIC-DATA\src\anonymization\anonymARXij"  # Change this if needed
 
-print(process.stdout)
+process = subprocess.run(["java", "-jar", jar_path], capture_output=True, text=True, cwd=working_directory)
+
+print("STDOUT:", process.stdout)
+print("STDERR:", process.stderr)
